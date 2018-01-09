7f03ba91d5f1285cc106b4eea945950ab86faac0
ENCALADA <encalada@de.ibm.com>
2018-01-09 21:46:39 +0100

    adding initial version of the script, only 2 API methods supported

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# kraken_api_script
-Script that interacts with the Kraken API for trading automation purposes
+## Kraken API ruby script (WIP)
+
+This is a ruby executable script that makes use of the [Kraken API documentation](https://www.kraken.com/help/api) for trading purposes.
+
+The script is based on the official Kraken ruby API repository, see [leishman/kraken_ruby](https://github.com/leishman/kraken_ruby) and it´s intended to be use as a standalone script.
+
+## Generate the API Key
+In your Kraken account , navigate to **Settings** >> **API**, and generate a new key. Adjust the key permissions according to your needs.
+
+This will generate two keys, an `api key` and `private key`.
+
+
+## Usage
+1. Make sure the script is executable, otherwise
+```
+chmod +x kraken_api.rb
+```
+2. Run it to see how to use it.
+```
+./kraken_api.rb
+This script interacts with the Kraken API, use it as follows:
+    -a, --api-key value              api-key (mandatory)
+    -p, --private-key value          api-private-key (mandatory)
+    -m, --method name                more info at https://www.kraken.com/help/api (default: TradeBalance())
+    -i, --method-input parameters    eg. asset:ZEUR (optional)
+```
+
+3. Provide the mandatory arguments. The `api_key` and the `private_key`.
+
+4. **OPTIONAL argument**. Set the desired method( e.g. `-m METHOD_NAME`, default is `TradeBalance`).
+
+  For the complete list of methods supported in this script(same names as in the API), refer to the **Current supported methods** section.
+
+  For the complete list of methods supported by the Kraken API, refer to the [Kraken API documentation](https://www.kraken.com/help/api) .
+
+5. **OPTIONAL argument**. Set the input values for the specified method( e.g. `-i 'pair:XETHZEUR'`). The `-i` expects an input in the form of `'key:value'`.
+To get a full description of the method inputs, please refer to the API Docs, where all API methods are described.
+
+
+## Current supported methods
+
+### Private methods
+TradeBalance
+
+### Public methods
+Ticker
+
+## TODO
+- Support all methods from the API into the script
+- Output of the request should be available in different formats(e.g. json, etc)
---
+++ kraken_api.rb
@@ -0,0 +1,160 @@
+#!/usr/bin/env ruby
+
+
+require 'base64'
+require 'securerandom'
+require 'addressable/uri'
+require 'optparse'
+require 'json'
+require 'uri'
+require 'net/http'
+
+
+$api_key = nil
+$api_secret = nil
+$api_version = "0"
+$base_uri = "https://api.kraken.com"
+$request_type = 'private'
+$method_name = 'TradeBalance'
+$method_input = Hash.new
+
+class String
+  def red;            "\e[31m#{self}\e[0m" end
+  def green;          "\e[32m#{self}\e[0m" end
+  def blue;           "\e[34m#{self}\e[0m" end
+  def bold;           "\e[1m#{self}\e[22m" end
+end
+
+def debug(string, stream=STDOUT)
+  stream.puts "Debug: ".bold + string.to_s
+end
+
+$optparse = OptionParser.new do | opts |
+  opts.banner = "This script interacts with the Kraken API, use it as follows:"
+  opts.on("-a", "--api-key value", String, "api-key (mandatory)") { |value| $api_key = value }
+  opts.on("-p", "--private-key value", String, "api-private-key (mandatory)") { |value| $api_secret = value }
+  opts.on("-m", "--method name", String, "more info at https://www.kraken.com/help/api (default: TradeBalance())") { |value| $method_name = value }
+  opts.on("-i", "--method-input parameters", String, "eg. asset:ZEUR (optional) ") { |value| $method_input = value }
+end
+
+$optparse.parse!
+def backout(message)
+  puts "\n"
+  puts $optparse.to_s + "\n" if defined? $optparse
+  abort("#{'ERROR:'} #{message}")
+end
+
+backout "No api-key specified" if $api_key == nil
+backout "No private-key specified" if $api_secret == nil
+
+def get_public(method, input={})
+  url = $base_uri + '/' + $api_version + '/public/' + method
+  body = encode_options(input)
+  uri = URI(url + '?' + body)
+  res = Net::HTTP.get_response(uri)
+  puts "Response #{res.code} ,  #{res.message}: , #{res.body}"
+  if res.code.eql?'200'
+    exit 0
+  else
+    exit 1
+  end
+end
+
+def post_private(method, input={})
+  input['nonce'] = generate_nonce
+  post_data = encode_options(input)
+  headers = { 'API-Key' => $api_key, 'API-Sign' => generate_message_signature(method, input, post_data) }
+  uri = URI.parse($base_uri + url_path(method))
+  https = Net::HTTP.new(uri.host,uri.port)
+  https.use_ssl = true
+  req = Net::HTTP::Post.new(uri.path, headers)
+  req.body = post_data
+  res = https.request(req)
+  puts "Response #{res.code} ,  #{res.message}: , #{res.body}"
+  if res.code.eql?'200'
+    exit 0
+  else
+    exit 1
+  end
+end
+
+# Sets the query component for this URI from a Hash object(POST data)
+def encode_options(input)
+  uri = Addressable::URI.new
+  uri.query_values = input
+  uri.query
+end
+
+# API-Sign = Message signature using HMAC-SHA512 of (URI path + SHA256(nonce + POST data)) and base64 decoded secret API ke
+def generate_message_signature(method, input={}, post_data)
+  key = Base64.decode64($api_secret)
+  message = generate_message(method, post_data, input)
+  generate_api_signature(key, message)
+end
+
+# Message formula is URI path + SHA256(nonce + POST data)
+def generate_message(method, post_data, input)
+  digest = OpenSSL::Digest.new('sha256', input['nonce'] + post_data).digest
+  url_path(method) + digest
+end
+
+def url_path(method)
+  '/' + $api_version + '/private/' + method
+end
+
+def generate_api_signature(key, message)
+  Base64.strict_encode64(OpenSSL::HMAC.digest('sha512', key, message))
+end
+
+
+def generate_nonce
+  #for the higher 48 bits, use the current timestamp to generate it
+  high_bits = (Time.now.to_f * 10000).to_i << 16
+  #for the 16 low bits, use the Bitwise AND operator to generate it
+  low_bits  = SecureRandom.random_number(2 ** 16) & 0xffff
+  #for the 64 bits, use the Bitwise OR operator, based on the value of the 16 and 48 bits numbers
+  (high_bits | low_bits).to_s
+end
+
+
+class Trade_Methods
+  #private methods
+  def output_debug(type, method, input={})
+    type.eql?("private") ? debug("Going to request => " + "private user data".bold.green) : debug("Going to request => " + "public market data.".bold.green )
+    debug("Requested method type => " + "#{method}".bold.green)
+    input.empty? ? debug("No inputs available for this method, using default values".red) : debug("Input: => " + "#{input}".blue)
+  end
+
+  #method names lose consistency, but I prefer them to be exactly as the names in the API docs, read https://www.kraken.com/help/api
+  def TradeBalance(input={})
+    output_debug("private", 'TradeBalance', input)
+    post_private 'TradeBalance', input
+  end
+
+  #public methods
+
+  #method names lose consistency, but I prefer them to be exactly as the names in the API docs, read https://www.kraken.com/help/api
+  def Ticker(input={})
+    output_debug("public", 'Ticker', input)
+    get_public 'Ticker', input
+  end
+end
+
+
+def process_input()
+  input_hash = Hash.new
+  return {} if $method_input.empty?
+  if $method_input.include?(',')
+    multiple_inputs = $method_input.split(',')
+  else
+    hash_values = $method_input.split(':')
+    input_hash = {
+      "#{hash_values[0]}" => "#{hash_values[1]}"
+    }
+
+  end
+  return input_hash
+end
+
+methods = Trade_Methods.new
+methods.send :"#{$method_name}" , process_input()
